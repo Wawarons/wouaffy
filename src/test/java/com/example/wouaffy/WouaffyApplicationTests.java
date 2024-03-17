abeb31d80c5ed593b76548a7ddb9a5c21744818b
Wawarons <adrien02.fr@gmail.com>
2024-03-17 23:41:22 +0100

    first commit

---
+++ src/test/java/com/example/wouaffy/WouaffyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wouaffy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WouaffyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
